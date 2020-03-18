4bff4e8f1cbd0c613c8275b805c8a9f572689285
DinuwanB <dinubhagya97@gmail.com>
2020-03-19 01:36:23 +0530

    numbers pattern add

--- src/CodeComplexDueToSize.java
+++ src/CodeComplexDueToSize.java
@@ -3,17 +3,19 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CodeComplexDueToSize {
     public static void main(String[] args) {
-
+        ArrayList<String> readWords = new ArrayList<String>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         ArrayList<String> keywords = new ArrayList<String>(Arrays.asList(
                 "abstract","assert","break","break" ,"class" ,"continue" ,"default" ,"enum", "extends" ,"final" ,
                 "finally" ,"implements" ,"import" ,"instanceof" ,"instanceof" ,"interface", "native", "new", "null" ,
                 "package", "private" ,"protected" ,"public", "return" ,"static" ,"strictfp" ,"super" ,"synchronized",
                 "this", "throw" , "throws" ,"transient" ,"try" ,"void" ,"volatile", "else"
         ));
-
         ArrayList<String> operators = new ArrayList<String>(Arrays.asList(
                 "+", "-", "*", "/", "%", "++", "--", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!",  "|", "^",
                 "~", "<<", ">>", ">>>", "<<<" , "->", ".", "::", "+=", "-=", "*=", "/=", "=", ">>>=", "|=", "&=" ,
@@ -22,24 +24,59 @@ public class CodeComplexDueToSize {
 
         try {
             File readData = new File("../Dinu/dinu.txt");
-            ArrayList<String> readWords = new ArrayList<String>();
             Scanner myReader = new Scanner(readData);
             while (myReader.hasNextLine()) {
                 String data = myReader.next();
                 readWords.add(data);
             }
-            myReader.close();
 
-            readWords.retainAll(operators);
+
             System.out.println(readWords);
+            //System.out.println(count);
+
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
+        numbersCount(readWords);
+        my1(readWords,keywords);
+        my2(readWords,operators);
+
+
+
 
     }
+
+    public static void my1(ArrayList<String> readwords, ArrayList<String> keywords){
+        System.out.println("my1  "+readwords);
+        readwords.retainAll(keywords);
+        System.out.println(readwords);
+    }
+
+    public static void my2(ArrayList<String> readwords, ArrayList<String> operators){
+        System.out.println("my2  " +readwords);
+        readwords.retainAll(operators);
+        int a = readwords.size();
+        System.out.println(readwords);
+        System.out.println(a);
+    }
+
+    public static void numbersCount(ArrayList<String> readwords){
+        Matcher mt;
+        Pattern p2 = Pattern.compile("[0-9][0-9]*");
+        mt = p2.matcher((CharSequence) readwords);
+        int count = 0;
+        while (mt.find()) {
+            count++;
+
+        }
+        System.out.println(count);
+    }
+
+
 }
+
 
 
