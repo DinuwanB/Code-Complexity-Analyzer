6855f4ba2473d1481edc09b61aa96e1db283df03
DinuwanB <dinubhagya97@gmail.com>
2020-03-25 02:09:22 +0530

    method complex class added

---
+++ src/CodeComplexDueToMethods.java
@@ -0,0 +1,11 @@
+public class CodeComplexDueToMethods {
+    public static void main(String[] args){
+        String filePath = "../Dinu/dinu.txt" ;
+
+        int result[] = MethodController(filePath);
+    }
+    public static int[] MethodController(String filepath){
+        System.out.println("Method class");
+        return null;
+    }
+}
