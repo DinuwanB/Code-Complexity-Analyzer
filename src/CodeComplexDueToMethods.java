f867ebc598a6bb5bb4e9f6606169ba8f01a86476
DinuwanB <dinubhagya97@gmail.com>
2020-03-25 23:12:30 +0530

    File Reader Added

--- src/CodeComplexDueToMethods.java
+++ src/CodeComplexDueToMethods.java
@@ -1,11 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CodeComplexDueToMethods {
     public static void main(String[] args){
         String filePath = "../Dinu/dinu.txt" ;
 
         int result[] = MethodController(filePath);
     }
-    public static int[] MethodController(String filepath){
-        System.out.println("Method class");
+    public static int[] MethodController(String filePath) {
+        try {
+            File readData = new File(filePath);
+            Scanner myReader = new Scanner(readData);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+
+        }catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         return null;
     }
 }
