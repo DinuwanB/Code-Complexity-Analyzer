4ad446e07f06b9d2316f6dee01c43fa1c72d3cc6
DinuwanB <dinubhagya97@gmail.com>
2020-03-29 00:18:31 +0530

    primitive return tpye method identify func created

--- src/CodeComplexDueToMethods.java
+++ src/CodeComplexDueToMethods.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CodeComplexDueToMethods {
     public static void main(String[] args){
@@ -9,22 +11,34 @@ public class CodeComplexDueToMethods {
         int result[] = MethodController(filePath);
     }
     public static int[] MethodController(String filePath) {
+
+        Matcher match;
         try {
             File readData = new File(filePath);
             Scanner myReader = new Scanner(readData);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+
+                Pattern primitiveReturnType = Pattern.compile(".(String|int|long|double|float|boolean|char)+.[a-zA-Z][a-zA-Z0-9]+\\(|[\\w_]+\\([a-zA-Z]*?\\);");
+                match = primitiveReturnType.matcher(data);
+                while (match.find()){
+                    System.out.println("Method Count  :" + match.group() );
+
+                }
+
+                Pattern compositeReturnType = Pattern.compile(".(String|int|long|double|float|boolean|char)+.[a-zA-Z][a-zA-Z0-9]+\\(|[\\w_]+\\([a-zA-Z]*?\\);");
+                match = compositeReturnType.matcher(data);
+                while (match.find()){
+                    System.out.println("Method Count  :" + match.group() );
+
+                }
+
             }
 
         }catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        System.out.println("An error occurred.");
-        System.out.println("An error occurred.");
-        System.out.println("An error occurred.");
-        System.out.println("An error occurred.");
         return null;
     }
 }
