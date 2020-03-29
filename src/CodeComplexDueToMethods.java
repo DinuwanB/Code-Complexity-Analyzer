8dcf5575cfdde5f3d6c163cca07aa5d587d5e6d8
DinuwanB <dinubhagya97@gmail.com>
2020-03-29 15:41:03 +0530

    paramter implementing

--- src/CodeComplexDueToMethods.java
+++ src/CodeComplexDueToMethods.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +15,13 @@ public class CodeComplexDueToMethods {
 
     }
     public static int[] MethodController(String filePath) {
+
+        String methData = "";
+        String forData = "";
+
+        ArrayList<String> identifyMethodsLine = new ArrayList<String>();
+        ArrayList<String> identifyForLine = new ArrayList<String>();
+
 
         Matcher match;
         try {
@@ -35,10 +44,19 @@ public class CodeComplexDueToMethods {
 
                 }
 
-                Pattern getMethodLine = Pattern.compile("public.+(void).+");
+                Pattern getMethodLine = Pattern.compile("\\(.+\\)");
                 match = getMethodLine.matcher(data);
                 while (match.find()){
-                    System.out.println("222  :" + match.group() );
+                    methData = match.group();
+                    identifyMethodsLine.add(methData);
+                   // System.out.println(identifyMethodsLine);
+                }
+
+                Pattern getVarInForLoop = Pattern.compile("(for).+");
+                match = getVarInForLoop.matcher(data);
+                while (match.find()){
+                    forData = match.group();
+                    identifyForLine.add(forData);
                 }
 
             }
@@ -47,6 +65,44 @@ public class CodeComplexDueToMethods {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+        ArrayList<String> primativeTypes = new ArrayList<String>(Arrays.asList("int","long","double","float","boolean","String"));
+        ArrayList<String> compositeTypes = new ArrayList<String>(Arrays.asList("int[]","long[]","double[]","float[]","boolean[]","String[]"));
+        ArrayList<String> forWords = new ArrayList<String>();
+        ArrayList<String> identMethWords = new ArrayList<String>();
+        ArrayList<String> identMethWords2 = new ArrayList<String>();
+
+        for (int counter = 0; counter < identifyForLine.size(); counter++) {
+            Pattern identifyinsideofForloopVariables = Pattern.compile("[a-zA-Z]+");
+            match = identifyinsideofForloopVariables.matcher(identifyForLine.get(counter));
+            while (match.find()){
+                String words = match.group();
+                forWords.add(words);
+            }
+        }
+        forWords.retainAll(primativeTypes);
+        System.out.println("111  :" +forWords.size());
+
+
+        for (int count = 0; count < identifyMethodsLine.size(); count++) {
+            Pattern insideofMethodVariablesPramCom = Pattern.compile("[a-zA-Z]+\\[\\]");
+            match = insideofMethodVariablesPramCom.matcher(identifyMethodsLine.get(count));
+            while (match.find()){
+                String words = match.group();
+                identMethWords.add(words);
+            }
+            Pattern identifyinsideofMethodVariables2 = Pattern.compile("[a-zA-Z]+");
+            match = identifyinsideofMethodVariables2.matcher(identifyMethodsLine.get(count));
+            while (match.find()){
+                String words2 = match.group();
+                identMethWords2.add(words2);
+            }
+        }
+        identMethWords.retainAll(compositeTypes);
+        System.out.println("333  :" +identMethWords);
+        identMethWords2.retainAll(primativeTypes);
+        System.out.println("444  :" +identMethWords2);
+
 
         return null;
     }
